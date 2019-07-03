1b208136692262f59ab028c4c94a87e1a6314137
Fabricio dos Santos Antunes <fabricio.antunes@bludata.local>
2019-07-03 14:07:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const TOKEN = process.env.TOKEN || 'NTk1OTYxNjg3NTM4OTI1NTg1.XRyncA._WqIL4wNlgh5rWPWvi35EyelTfY';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+const manageevent = (eventName, event ) => {
+    if (event.d.message_id === '595972412819111986') {
+        const reactionChannel = client.channels.get(event.d.channel_id);
+        if (reactionChannel.messages.has(event.d.message_id)) return;
+        else {
+            reactionChannel.fetchMessage(event.d.message_id)
+                .then(msg => {
+                    var msgReaction = msg.reactions.get(event.d.emoji.name + ":" + event.d.emoji.id);
+                    if (msgReaction === undefined)
+                        msgReaction = msg.reactions.get(event.d.emoji.name);
+
+                    if (msgReaction !== undefined) {
+                        var user = client.users.get(event.d.user_id);
+                        client.emit(eventName, msgReaction, user)
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+    }
+}
+
+client.on('raw', event => {
+    const eventName = event.t;
+    switch (eventName) {
+        case 'MESSAGE_REACTION_REMOVE':
+        case 'MESSAGE_REACTION_ADD':
+            manageevent(eventName,event);
+            break;
+    }
+
+
+});
+
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    var roleName = messageReaction.emoji.name.toLowerCase();
+    var role = messageReaction.message.guild.roles.find(role => role.name.toLocaleLowerCase() === roleName);
+    if (role) {
+        var member = messageReaction.message.guild.members.find(member => member.id === user.id)
+        member.addRole(role.id)
+    }
+});
+
+client.on('messageReactionRemove', (messageReaction, user) => {
+    var roleName = messageReaction.emoji.name.toLowerCase();
+    var role = messageReaction.message.guild.roles.find(role => role.name.toLocaleLowerCase() === roleName);
+    if (role) {
+        var member = messageReaction.message.guild.members.find(member => member.id === user.id)
+        member.removeRole(role.id)
+    }
+});
+
+client.login(TOKEN);
